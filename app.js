04130fe8b5e9029adafcdabdbf7570c5a6497aba
mcoons <mcoons67@gmail.com>
2018-09-20 13:47:15 -0600

    added routes

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 // require/import the express module(s)
+
+// https://agile-citadel-53491.herokuapp.com/
+
 const express = require('express'); 
 const queries = require("./queries");
 const database = require("./database-connection");
@@ -24,31 +27,31 @@ app.get('/', (request, response, next) => response.send('Hello There'));
 
 
 app.get("/api/", (request, response, next) => {
-    queries
-      .list()
-      .then(scores => { response.json({ scores }); })
-      .catch(next);
-  });
-  
-  app.get("/api/:id", (request, response, next) => {
-    queries
-      .read('id', request.params.id)
-      .then(score => { response.json({score}); })
-      .catch(next);
-  });
-  
-  app.post("/api/", (request, response, next) => {
-    queries.create(request.body).then(score => {
-        response.status(201).json({score: score});
-    }).catch(next);
-  });
+  queries
+    .list()
+    .then(scores => { response.json({ scores }); })
+    .catch(next);
+});
+
+app.get("/api/:id", (request, response, next) => {
+  queries
+    .read('id', request.params.id)
+    .then(score => { response.json({score}); })
+    .catch(next);
+});
+
+app.post("/api/", (request, response, next) => {
+  queries.create(request.body).then(score => {
+    response.status(201).json({score: score});
+  }).catch(next);
+});
 
 
-  app.delete("/api/:id", (request, response, next) => {
-    queries.delete(request.params.id).then(() => {
-        response.status(204).json({deleted: true});
-    }).catch(next);
-  });
+app.delete("/api/:id", (request, response, next) => {
+  queries.delete(request.params.id).then(() => {
+    response.status(204).json({deleted: true});
+  }).catch(next);
+});
 
 
 
