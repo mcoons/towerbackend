d33b609647ac94f28c6f0bffad2621ddf684d2bb
mcoons <mcoons67@gmail.com>
2018-09-20 13:54:10 -0600

    capitolized port in app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const localport = 3030;
 
 // use the providers (heroku) port variable if there is one (production)
 // otherwise use our local port
-const port = process.env.port || localport;
+const port = process.env.PORT || localport;
 
 // create a base route to direct root GET requests to
 app.get('/', (request, response, next) => response.send('Hello There'));    
